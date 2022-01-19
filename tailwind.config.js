170b7ab4d968f3a40cf4b4f3f8282b4e95d8b6fb
DanteBorges <danielnorges@gmail.com>
2022-01-19 18:45:20 -0300

    added @tailwindcss/typography and @tailwindcss/aspect-ratio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
 module.exports = {
   purge: [
-    './src/**/*.html',
-    './src/**/*.js',
-    './src/**/*.jsx',
-    './src/**/*.ts',
-    './src/**/*.tsx',
-    './public/index.html',
+    "./src/**/*.html",
+    "./src/**/*.js",
+    "./src/**/*.jsx",
+    "./src/**/*.ts",
+    "./src/**/*.tsx",
+    "./public/index.html",
   ],
   theme: {
     extend: {
       colors: {
-        'midnightblue': '#2f2e7c',
-        'royalblue':'#4f46e5',
-        'darkslateblue': '#234894',
-        'steelblue': '#3b85c1',
-        'slategray': '#8f5ba3',
-        'plum': '#cfa6ce',
-      }
-     },
+        midnightblue: "#2f2e7c",
+        royalblue: "#4f46e5",
+        darkslateblue: "#234894",
+        steelblue: "#3b85c1",
+        slategray: "#8f5ba3",
+        plum: "#cfa6ce",
+      },
+    },
   },
   variants: {},
-  plugins: []
-}
+  plugins: [],
+};
