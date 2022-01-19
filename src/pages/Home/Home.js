d7ef6c5b07535fd15b46af89828d0daa3be7ff6b
DanteBorges <danielnorges@gmail.com>
2022-01-19 18:37:42 -0300

    Hire me added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../../components/Navbar"
 import Hero from "../../components/Hero"
+import HireMe from "../../components/HireMe/"
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <HireMe/>
     </>
   );
 };
