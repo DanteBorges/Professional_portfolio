348626c18399509a0204025bd975c71ee011944b
DanteBorges <danielnorges@gmail.com>
2022-01-25 18:03:22 -0300

    portifolio page added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "../../components/Navbar"
 import Hero from "../../components/Hero"
+import Portifolio from "../../components/Portifolio"
+
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Portifolio/>
     </>
   );
 };
