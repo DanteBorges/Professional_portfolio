681cb41cd3af56446f75945064af9cf25fc3f756
DanteBorges <danielnorges@gmail.com>
2022-02-02 13:43:53 -0300

    link hire me destination removed from footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,7 +9,7 @@ const navigation = {
     { name: "Home", href: "#home" },
     { name: "About me", href: "#about" },
     { name: "Projects", href: "#projects" },
-    { name: "Hire me", href: "#hireme" },
+  
   ],
   social: [
  
